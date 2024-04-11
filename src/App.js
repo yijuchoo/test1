4246ada92e227db6109d415bfd9e80935b91d015
Yirou C. <yirou26@msn.com>
2024-04-12 00:21:01 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import PrintMsg from "./components/helloworld";
+
+function App() {
+  return (
+    <>
+      <h1>This is a test demo</h1>
+      <PrintMsg />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/helloworld.jsx
@@ -0,0 +1,8 @@
+export default function PrintMsg(){
+    return(
+        <>
+        <h2>Hello World</h2>
+        <p>This is just printing some text to test and upload in GitHub.</p>
+        </>
+    );
+}
